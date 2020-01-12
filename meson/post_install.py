d91eb5b4aabaf63fecce2bd7fba9edfefdf2a5dc
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2020-01-12 10:08:46 +0100

    This time it should be executable

--- meson/post_install.py
+++ meson/post_install.py
@@ -8,4 +8,3 @@ schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0'
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas…')
     subprocess.call(['glib-compile-schemas', schemadir], shell=False)
-
